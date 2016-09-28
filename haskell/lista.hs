b252caa2c0c8ded1e0da5d3d97f2fba5b34afa5a
Raí Santos da Soledade <raisoledade@outlook.com>
2016-09-28 17:37:40 -0400

    Update lista.hs

--- haskell/lista.hs
+++ haskell/lista.hs
@@ -13,6 +13,11 @@ fib n = fib(n-1) + fib(n-2)
 --Fim Questao 2
 --------------------------------------------------------------
 --Questao 3 -> Uniao
+esta_em x [] = False								--Funçao que recebe um valor e um vetor
+esta_em x (y:ys)
+    |x == y = True
+    |otherwise = esta_em x ys
+
 uniao:: [Int] -> [Int] -> [Int]								--Pega duas listas e une
 uniao (as) (b:bs)
 	|not (esta_em b as)= uniao as bs ++ b  					--Se o item 'b' nao estiver na lista 'as', ele acrescenta no vetor que será retornado
@@ -20,6 +25,11 @@ uniao (as) (b:bs)
 --Fim Questao 3        
 --------------------------------------------------------------
 --Questao 4 -> Intersecçao
+esta_em x [] = False								--Funçao que recebe um valor e um vetor
+esta_em x (y:ys)
+    |x == y = True
+    |otherwise = esta_em x ys
+    
 interseccao xs [] = []										--Pega duas listas e retorna uma lista com numeros presentes nas duas 
 interseccao xs (y:ys)										
     |esta_em y xs == False = interseccao xs ys				--Se o item 'y' estiver na lista 'xs', tenta com o proximo elemento
@@ -51,6 +61,18 @@ fatores n = [i | i <- [1..n] , mod n i == 0]		-- cria uma lista com todos os div
 --Fim Questao 8
 --------------------------------------------------------------
 --Questao 9 -> Quadrado Perfeito
+func n = [quadrado i | i <- [1..n]]					-- cria um vetor com o quadrado de todos os numero de 1..N
+
+quadrado :: (Integral a) => a -> a                  -- calcula recursivamente o quadrado de um numero
+quadrado 1 = 1
+quadrado n = quadrado (n-1) + (n-1) + n 
+
+item_lista :: (Integral a) => [a] -> a -> Bool 		-- verifica se um item esta contido na lista
+item_lista [] _ = False 							-- se for uma lista vazia retorna false
+item_lista (x:xs) a = if x == a                     -- se a head for igual ao elemento retorna true, senao repete a funçao com a 
+			then True 					-- cauda da lista
+			else item_lista xs a
+
 raiz_quadrada ::(Integral a) => a -> Bool 			-- verifica se o elemento possui raiz quadrada
 raiz_quadrada 1 = True
 raiz_quadrada a = item_lista (func a) a	
