2109397759fdb02a457c5259f6fc3ea0b2b31660
Raí Santos da Soledade <raisoledade@outlook.com>
2016-09-28 17:23:58 -0400

    Update lista.hs

--- haskell/lista.hs
+++ haskell/lista.hs
@@ -1,148 +1,83 @@
-1º Questão - Fatorial
+--Questao 1 -> Fatorial
+fat :: (Integral x) => x -> x 								--calcula o fatorial recursivamente.
+fat 0 = 1													--fatorial de um numero é igual ao numero multiplicado pelo
+fat n = n * fat(n - 1)										--fatorial do anterior.
+--Fim Questao 1
+--------------------------------------------------------------
+--Questao 2 -> Fibonacci
+fib :: (Integral x) => x -> x 								--Calcula o N numero de fibonacci recursivamente
+fib 1 = 0													--Casos base
+fib 2 = 1													--O N-ésimo numero de fibonacci é igual ao seus dois antecessores.
+fib 3 = 1
+fib n = fib(n-1) + fib(n-2)
+--Fim Questao 2
+--------------------------------------------------------------
+--Questao 3 -> Uniao
+uniao:: [Int] -> [Int] -> [Int]								--Pega duas listas e une
+uniao (as) (b:bs)
+	|not (esta_em b as)= uniao as bs ++ b  					--Se o item 'b' nao estiver na lista 'as', ele acrescenta no vetor que será retornado
+    |otherwise = uniao as bs as								--Se não, simplesmente tenta com o proximo elemento
+--Fim Questao 3        
+--------------------------------------------------------------
+--Questao 4 -> Intersecçao
+interseccao xs [] = []										--Pega duas listas e retorna uma lista com numeros presentes nas duas 
+interseccao xs (y:ys)										
+    |esta_em y xs == False = interseccao xs ys				--Se o item 'y' estiver na lista 'xs', tenta com o proximo elemento
+    |otherwise = interseccao xs ys ++ [y]					--Se nao, acrescenta o item.
+--Fim Questao 4
+--------------------------------------------------------------
+--Questao 5 -> Roda Esquerda								
+rodar_esquerda x (ys)										--Recebe um valor 'x' e um lista 'ys' e roda a lista 'ys' a esquerda 'x' vezes. 
+    | x == 0 = ys											--Caso base, se 'x' for igual 0, nao roda a lista
+    |otherwise = rodar_esquerda (x-1) (tail ys ++ [head ys])--roda a esquerda 'x-1' vezes com o primeiro elemento concatenado no fim da cauda da lista.
+--Fim Questao 5
+--------------------------------------------------------------
+--Questao 6 -> Roda Direita    
+rodar_direita x (ys)										--Recebe um valor 'x' e um lista 'ys' e roda a lista 'ys' a direita 'x' vezes.
+    | x == 0 = ys											--Caso base, se 'x' for igual 0, nao roda a lista
+    |otherwise = rodar_direita (x-1) ([last ys] ++ init ys) --roda a direita 'x-1' vezes com o ultimo elemento concatenado com a lista menos o ultimo elemento
+--Fim Questao 6
+--------------------------------------------------------------
+--Questao 7 -> Seleciona 
+seleciona :: [Char] -> [Int] -> [Char]						--Recebe uma lista de Char e uma lista de Inteiros e retorna uma lista com os Char nas posiçoes dadas pela lista de Inteiros
+seleciona a b = [a !! (x-1) | x <- b]						--Copia o Char na lista 'a' na posiçao 'x-1' onde esse 'x' é um elemento da lista 'b'
+--Fim Questao 7
+--------------------------------------------------------------
+--Questao 8 -> Primo
+primo n = if(fatores n) == [1,n] 					-- se a lista de divisores for igual a [1,n] entao N é primo
+			then True
+			else False
+fatores n = [i | i <- [1..n] , mod n i == 0]		-- cria uma lista com todos os divisores de n
+--Fim Questao 8
+--------------------------------------------------------------
+--Questao 9 -> Quadrado Perfeito
+raiz_quadrada ::(Integral a) => a -> Bool 			-- verifica se o elemento possui raiz quadrada
+raiz_quadrada 1 = True
+raiz_quadrada a = item_lista (func a) a	
+--Fim Questao 9
+--------------------------------------------------------------
+--Questao 10 -> Conjunto das Partes
+subsets [] = [[]]									--Recebe uma lista e retorna uma lista com todas as partiçoes da lista recebida
+subsets (x:xs) = 									
+	let subsets_xs = subsets xs						--
+    in subsets_xs ++ [(x:z) | z <- subsets_xs]		--
+--Fim Questao 10
+--------------------------------------------------------------
+--Funçoes Auxiliares
+esta_em x [] = False								--Funçao que recebe um valor e um vetor
+esta_em x (y:ys)
+    |x == y = True
+    |otherwise = esta_em x ys
+    
+func n = [quadrado i | i <- [1..n]]					-- cria um vetor com o quadrado de todos os numero de 1..N
 
-(defun factorial (num)
-   (cond ((zerop num) 1)
-      (t ( * num (factorial (- num 1))))
-   )
-)
-(setq n 6)
-(format t "~% Factorial ~d is: ~d" n (factorial n))
+quadrado :: (Integral a) => a -> a                  -- calcula recursivamente o quadrado de um numero
+quadrado 1 = 1
+quadrado n = quadrado (n-1) + (n-1) + n 
 
-------------------------------------------------------------------------------------------------------------------------------
-2º Questão  Sequência Fibonacci
-(defun fibonacci (num)
-    (cond ((= num 1) 0)
-        ((= num 2) 1)	
-            (t (+ (fibonacci(- num 1)) (fibonacci (- num 2))))
-    )
-)
-(setq n 5)
-(format t "~% The ~d number in the Fibonacci Sequence is: ~d" n (fibonacci n))
-
------------------------------------------------------------------------------------------------------------
-3º Questão  - União 
-((defun uniao (l1 l2)
-    (setq luniao (copy-seq l2))
-    (mapcar 
-        (lambda (x y) 
-        (cond ((not(position x l2)) (pushnew x luniao))))
-        l1 l2)
-    (sort luniao '<)
-    (return-from uniao luniao)
-)
-
-(setq a (list 1 2 3 ))
-(setq b (list 3 4 5 1))
-(write (uniao a b))
-
-------------------------------------------------------------------------------------------------------------------------------
-4º Questão -  	
-(defun intersecao (l1 l2)
-    (setq lintersecao (list))
-    (mapcar 
-        (lambda (x y) 
-        (cond ((position x l2) (pushnew x lintersecao))))
-        l1 l2)
-    (sort lintersecao '<)
-    (return-from intersecao lintersecao)
-)
-
-(setq a (list 1 2 3 ))
-(setq b (list 3 4 5 1))
-(write (intersecao a b))
-
-------------------------------------------------------------------------------------------------------------------------------
-5º Questão - RodarEsquerda
-
-defun rodarEsquerda ( num lista)
-	(dotimes (i num)
-	    (setq ultimap (- (length lista) 1))
-	    (setq anterior(elt lista ultimap))
-	    (setq atual nil)
-	    (loop for j from 1 to ultimap
-		    do
-		       	   (setq posicao (- ultimap j))
-			    (setq atual (elt lista posicao))
-			    (setf (elt lista posicao) anterior)
-			    (setq anterior atual)
-        )
-         (setf (elt lista ultimap) atual)
-    )
-    (return-from rodarEsquerda lista)
-)
-
-(setq a (list 1 2 3))
-(write (rodarEsquerda 2 a ))
------------------------------------------------------------------------------------------------------------
-6º Questão – RodarDireita
-
-(defun rodarDireita( num lista)
-	(dotimes (i num)
-	    (setq ultimap (- (length lista) 1))
-	    (setq primeirap 0)
-	    (setq anterior(elt lista primeirap))
-	    (setq atual nil)
-	    (loop for j from 1 to ultimap
-		    do
-		        (setq posicao j)
-			    (setq atual (elt lista posicao))
-			    (setf (elt lista posicao) anterior)
-			    (setq anterior atual)
-        )
-         (setf (elt lista primeirap) atual)
-    )
-    (return-from rodarDireita lista)
-)
-
-(setq a (list 1 2 3))
-(write (rodarDireita 2 a ))
-
-
-
-
-
-
-
-
-
-
-7º Questão –  vetorSeleção
-
-(defun selecao (lista listaindice)
-        (setq ultimap ( - (length listaindice) 1))
-        (setq listaselecao (list))
-	    (loop for j from 0 to ultimap
-		    do
-		        (setq posicao (- ultimap j))
-			    (setq listaselecao (cons (elt lista (elt listaindice posicao)) listaselecao))
-        )
-        (return-from selecao listaselecao)
-)
-
-(setq l (list 3 22 15 17 18))
-(setq posicao (list 0 0 1 1 2 2 3 3 4 4))
-(write (selecao l posicao))
-
-8º Questão Primo 
-
-(defun primo (num)
-        (setq count 0)
-	    (loop for i from 2 to (- num 1)
-		    do
-		        (if ( = (mod num i) 0)
-		            (setq count (+ count 1))
-		        )
-        )
-        (return-from primo (= count 0))
-)
-(setq num 5)
-(write (primo num)) 
-
-Questão 10 
-(defun combos (list)
-	(if (null list) '(nil)
-	(let * (( a (car list))
-		(d (cdr list))
-		(s (combos d))
-		(v (mapcar (lambda (x) (cons a x )) s )))
-		(append s v ))))
+item_lista :: (Integral a) => [a] -> a -> Bool 		-- verifica se um item esta contido na lista
+item_lista [] _ = False 							-- se for uma lista vazia retorna false
+item_lista (x:xs) a = if x == a                     -- se a head for igual ao elemento retorna true, senao repete a funçao com a 
+				  		then True 					-- cauda da lista
+						else item_lista xs a
+--Fim Funçoes Auxiliares
